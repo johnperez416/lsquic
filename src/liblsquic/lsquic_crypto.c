johnperez416/lsquic

commit 13811f7055f61da9f7cb1e379b889d9b369c689d

Fix up sha256, lshkdf_* and lsquic_c255_* apis to be compatible with openssl (#557)
* Update target boringssl version

The boringssl version that lsquic compiles with varies in ci targets and
is quite old.  Specifically it is lacking some api support that allows
for reasonable compatibility with current versions of openssl.  Update
to the latest release to complete compatibility.

* modify lshkdf_expand to use openssl compatible api

the lshkdf_expand function use an hkdf api that is unique to boringssl.
In order to make it amenable to using openssl at well, we need to
convert it to use apis that exist in both implementations

* convert sha256 function to use compatible openssl api

The sha256 function uses a api that is unique to boringssl.  Convert it
to an api that exists in both boringssl and openssl so that either
implementation can be used

* use compatible api for x25519 key deriviation

The x25519 api in lsquic_crypto.c uses a custom boringssl api.  Update
it to use a compatible api that works in both boring and openssl

* update appveyor to use windows visual studio 2019

When updating to a more recent boringssl to pull in more recent api
updates, the boringssl build broke, as visual studio 2017 doesn't
support the _udiv128 intrinsic that boringssl requires.  We should
update to at least VS 2019 images as:
1) VS 2017 is going EOL in 2027
2) Not updating prevents pulling in newer boringssl versions that fix
   security issues

* Update check_symbol_exists for older compilers

It appears older compilers (like on ubuntu 20.04) don't resolve symbols
properly when -l options are specified before static libraries, leading
to failed symbols resolution and configuration errors

Update the check_symbol_exists code in CMakeLists.txt to ensure that
-pthread and -lstdc++ get added to the end of the command line

* Fix windows build with new boringssl

Windows builds are failing with the updated boringssl as boring uses

src/liblsquic/lsquic_crypto.c
@@ -7,8 +7,8 @@
 #include <openssl/stack.h>
 #include <openssl/x509.h>
 #include <openssl/rand.h>
-#include <openssl/curve25519.h>
-#include <openssl/hkdf.h>
+#include <openssl/evp.h>
+#include <openssl/kdf.h>
 #include <openssl/hmac.h>
 
 #include <zlib.h>
@@ -167,12 +167,28 @@ void lsquic_serialize_fnv128_short(uint128 v, uint8_t *md)
 #endif
 
 
-static void sha256(const uint8_t *buf, int len, uint8_t *h)
+static int sha256(const uint8_t *buf, int len, uint8_t *h)
 {
-    SHA256_CTX ctx;
-    SHA256_Init(&ctx);
-    SHA256_Update(&ctx, buf, len);
-    SHA256_Final(h, &ctx);
+    EVP_MD_CTX *ctx = EVP_MD_CTX_new();
+    unsigned int outlen = 0;
+    int ret = -1;
+
+    if (ctx == NULL)
+        return -1;
+
+    if (!EVP_DigestInit(ctx, EVP_sha256()))
+        goto err;
+
+    if (!EVP_DigestUpdate(ctx, buf, len))
+        goto err;
+
+    if (!EVP_DigestFinal(ctx, h, &outlen))
+        goto err;
+
+    ret = 0;
+err:
+    EVP_MD_CTX_free(ctx);
+    return ret;
 }
 
 
@@ -206,7 +222,7 @@ int lshkdf_expand(const unsigned char *prk, const unsigned char *info, int info_
                 uint16_t sub_key_len, uint8_t *sub_key,
                 uint8_t *c_hp, uint8_t *s_hp)
 {
-    const unsigned L = c_key_len + s_key_len + c_key_iv_len + s_key_iv_len
+    size_t L = c_key_len + s_key_len + c_key_iv_len + s_key_iv_len
             + sub_key_len
             + (c_hp ? c_key_len : 0)
             + (s_hp ? s_key_len : 0)
@@ -218,14 +234,35 @@ int lshkdf_expand(const unsigned char *prk, const unsigned char *info, int info_
       + 32                      /* Subkey */
       + EVP_MAX_KEY_LENGTH * 2  /* Header protection */
     ];
+    EVP_PKEY_CTX *ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_HKDF, NULL);
+    int ret = -1;
 
     assert((size_t) L <= sizeof(output));
 
-#ifndef NDEBUG
-    const int s =
+    if (ctx == NULL)
+        goto err;
+    
+    if (EVP_PKEY_derive_init(ctx) <= 0)
+        goto err;
+#ifdef HAVE_BORINGSSL
+    if (EVP_PKEY_CTX_hkdf_mode(ctx, EVP_PKEY_HKDEF_MODE_EXPAND_ONLY) <= 0)
+        goto err;
+#else
+    if (EVP_PKEY_CTX_set_hkdf_mode(ctx, EVP_PKEY_HKDEF_MODE_EXPAND_ONLY) <= 0)
+        goto err;
 #endif
-    HKDF_expand(output, L, EVP_sha256(), prk, 32, info, info_len);
-    assert(s);
+    if (EVP_PKEY_CTX_set_hkdf_md(ctx, EVP_sha256()) <= 0)
+        goto err;
+
+    if(EVP_PKEY_CTX_set1_hkdf_key(ctx, prk, 32) <= 0)
+        goto err;
+
+    if (EVP_PKEY_CTX_add1_hkdf_info(ctx, info, info_len) <= 0)
+        goto err;
+
+    if (EVP_PKEY_derive(ctx, output, &L) <= 0)
+        goto err;
+
     p = output;
     if (c_key_len)
     {
@@ -262,7 +299,10 @@ int lshkdf_expand(const unsigned char *prk, const unsigned char *info, int info_
         memcpy(s_hp, p, s_key_len);
         p += s_key_len;
     }
-    return 0;
+    ret = 0;
+err:
+    EVP_PKEY_CTX_free(ctx);
+    return ret;
 }
 
 
@@ -318,13 +358,44 @@ lsquic_export_key_material(const unsigned char *ikm, uint32_t ikm_len,
 
 void lsquic_c255_get_pub_key(unsigned char *priv_key, unsigned char pub_key[32])
 {
-    X25519_public_from_private(pub_key, priv_key);
+    int len = 32;
+    size_t outlen;
+    EVP_PKEY *key = EVP_PKEY_new_raw_private_key(EVP_PKEY_X25519, NULL, priv_key, len);
+    
+    if (key != NULL)
+        EVP_PKEY_get_raw_public_key(key, pub_key, &outlen);
+    EVP_PKEY_free(key);
 }
 
 
 int lsquic_c255_gen_share_key(unsigned char *priv_key, unsigned char *peer_pub_key, unsigned char *shared_key)
 {
-    return X25519(shared_key, priv_key, peer_pub_key);
+    int ret = -1;
+    int len = 32;
+    size_t outlen;
+    EVP_PKEY *my_key = EVP_PKEY_new_raw_private_key(EVP_PKEY_X25519, NULL, priv_key, len);
+    EVP_PKEY *pub_key = EVP_PKEY_new_raw_public_key(EVP_PKEY_X25519, NULL, peer_pub_key, len);
+    EVP_PKEY_CTX *ctx = NULL;
+
+    if (my_key == NULL || pub_key == NULL)
+        goto err;
+
+    ctx = EVP_PKEY_CTX_new(my_key, NULL);
+    if (ctx == NULL)
+        goto err;
+
+    if (!EVP_PKEY_derive_init(ctx))
+        goto err;
+    if (!EVP_PKEY_derive_set_peer(ctx, pub_key))
+        goto err;
+    if (!EVP_PKEY_derive(ctx, shared_key, &outlen))
+        goto err;
+    ret = 0;
+err:
+    EVP_PKEY_CTX_free(ctx);
+    EVP_PKEY_free(pub_key);
+    EVP_PKEY_free(my_key);
+    return ret;
 }
 
 
@@ -437,7 +508,9 @@ lsquic_gen_prof (const uint8_t *chlo_data, size_t chlo_data_len,
     EVP_MD_CTX sign_context;
     EVP_PKEY_CTX* pkey_ctx = NULL;
     
-    sha256(chlo_data, chlo_data_len, chlo_hash);
+    if (sha256(chlo_data, chlo_data_len, chlo_hash) != 0)
+        return -1;
+
     EVP_MD_CTX_init(&sign_context);
     if (!EVP_DigestSignInit(&sign_context, &pkey_ctx, EVP_sha256(), NULL, (EVP_PKEY *)priv_key))
         return -1;
@@ -480,7 +553,9 @@ verify_prof0 (const uint8_t *chlo_data, size_t chlo_data_len,
     EVP_PKEY_CTX* pkey_ctx = NULL;
     int ret = 0;
     EVP_MD_CTX_init(&sign_context);
-    sha256(chlo_data, chlo_data_len, chlo_hash);
+    
+    if (sha256(chlo_data, chlo_data_len, chlo_hash) != 0)
+        return -3;
     
     // discarding const below to quiet compiler warning on call to ssl library code
     if (!EVP_DigestVerifyInit(&sign_context, &pkey_ctx, EVP_sha256(), NULL, (EVP_PKEY *)pub_key))

.cirrus.yml
@@ -8,7 +8,7 @@ task:
         - cd boringssl
         # This is so that both GQUIC and IETF branches build.  Just picking
         # a known good revision:
-        - git checkout a2278d4d2cabe73f6663e3299ea7808edfa306b9
+        - git checkout 0.20250807.0
         - cmake .
         - make
         - cd -

CMakeLists.txt
@@ -119,6 +119,7 @@ SET(MY_CMAKE_FLAGS "${MY_CMAKE_FLAGS} /wd4459")	# hide global declaration
 SET(MY_CMAKE_FLAGS "${MY_CMAKE_FLAGS} /wd4706")	# assignment within conditional expression
 SET(MY_CMAKE_FLAGS "${MY_CMAKE_FLAGS} /wd4090")	# different 'const' qualifier (TODO: debug ls-sfparser.c)
 SET(MY_CMAKE_FLAGS "${MY_CMAKE_FLAGS} /wd4305")	# truncation from double to float
+SET(MY_CMAKE_FLAGS "${MY_CMAKE_FLAGS} /wd4201") # Allow nameless structs
 SET(MY_CMAKE_FLAGS "${MY_CMAKE_FLAGS} -W4 -WX -Zi -DWIN32_LEAN_AND_MEAN -DNOMINMAX -D_CRT_SECURE_NO_WARNINGS -I${CMAKE_CURRENT_SOURCE_DIR}/wincompat")
 IF(LSQUIC_SHARED_LIB)
     SET(MY_CMAKE_FLAGS "${MY_CMAKE_FLAGS} -DLSQUIC_SHARED_LIB")
@@ -239,7 +240,7 @@ ENDIF()
 if ("${STATIC_BUILD}" STREQUAL "yes")
     SET(CMAKE_REQUIRED_INCLUDES "${SSLLIB_INCLUDE}")
     SET(CMAKE_REQUIRED_LIBRARIES "${LIBSSL_LIB_ssl};${LIBSSL_LIB_crypto}")
-    SET(CMAKE_EXE_LINKER_FLAGS "-pthread -lstdc++")
+    LIST(APPEND CMAKE_REQUIRED_LIBRARIES "-pthread -lstdc++")
 ELSE()
     SET(CMAKE_EXE_LINKER_FLAGS "-Wl,-rpath=${LIBSSL_DIR} -L${LIBSSL_DIR} -lssl -lcrypto")
 ENDIF()

appveyor-linux.yml
@@ -25,7 +25,7 @@ install:
 
         cd boringssl
 
-        git checkout cf8d3ad3cea51cf7184307d54f465da62b7d8408
+        git checkout 0.20250807.0
 
         cmake .
 

appveyor-windows.cmd
@@ -5,8 +5,8 @@ if exist ".\boringssl\CMakeLists.txt" (
 ) else (
     git clone https://boringssl.googlesource.com/boringssl
     cd boringssl
-    git checkout cf8d3ad3cea51cf7184307d54f465da62b7d8408
-    cmake -DCMAKE_GENERATOR_PLATFORM=x64 --config Debug -DBUILD_SHARED_LIBS=OFF -DOPENSSL_NO_ASM=1 .
+    git checkout 0.20250807.0
+    cmake -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=OFF -DOPENSSL_NO_ASM=1 .
     msbuild /m ALL_BUILD.vcxproj
     cd ..
 )

appveyor-windows.yml
@@ -1,6 +1,6 @@
 version: 1.{branch}.{build}
 
-image: Visual Studio 2017
+image: Visual Studio 2019
 
 init:
 

commit d5929af7cec6fd74f1cfea2cb1c07c27ce9102b1

Fix: initialize gQUIC X25519 output lengths (#694)
Fixes regression introduced in 13811f7055f61da9f7cb1e379b889d9b369c689d

src/liblsquic/lsquic_crypto.c
@@ -359,7 +359,7 @@ lsquic_export_key_material(const unsigned char *ikm, uint32_t ikm_len,
 void lsquic_c255_get_pub_key(unsigned char *priv_key, unsigned char pub_key[32])
 {
     int len = 32;
-    size_t outlen;
+    size_t outlen = 32;
     EVP_PKEY *key = EVP_PKEY_new_raw_private_key(EVP_PKEY_X25519, NULL, priv_key, len);
     
     if (key != NULL)
@@ -372,7 +372,7 @@ int lsquic_c255_gen_share_key(unsigned char *priv_key, unsigned char *peer_pub_k
 {
     int ret = -1;
     int len = 32;
-    size_t outlen;
+    size_t outlen = 32;
     EVP_PKEY *my_key = EVP_PKEY_new_raw_private_key(EVP_PKEY_X25519, NULL, priv_key, len);
     EVP_PKEY *pub_key = EVP_PKEY_new_raw_public_key(EVP_PKEY_X25519, NULL, peer_pub_key, len);
     EVP_PKEY_CTX *ctx = NULL;